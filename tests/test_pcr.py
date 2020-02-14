fc5d38f5095316a6a98683adb7185e76f399098e
peio <pziarsolo@upv.es>
2020-02-14 11:16:45 +0100

    clean unused tests

--- tests/test_pcr.py
+++ tests/test_pcr.py
@@ -10,38 +10,7 @@ TEST_DATA_PATHDIR = Path(primer_explorer.__file__).parent.parent.joinpath('tests
 
 class TestPcrProducts(unittest.TestCase):
 
-    def xtest_get_pcr_products(self):
-        kmer_locations = {
-            b'GGTAGGAT': [KmerAndLocation(seq=b'GGTAGGAT', chrom_location=(b'chrom1', 0), is_heterochromatic=False),
-                          KmerAndLocation(seq=b'GGTAGGAT', chrom_location=(b'chrom1', 5), is_heterochromatic=False)],
-            b'ATCCTACC': [KmerAndLocation(seq=b'ATCCTACC', chrom_location=(b'chrom1', 20), is_heterochromatic=True)],
-
-#                           KmerAndLocation(seq=b'ATCCTACC', chrom_location=(b'chrom2', 3), is_heterochromatic=True),
-#                           KmerAndLocation(seq=b'ATCCTACC', chrom_location=(b'chrom1', 60), is_heterochromatic=True)],
-
-            b'GTAGGGCA': [KmerAndLocation(seq=b'GTAGGGCA', chrom_location=(b'chrom1', 30000), is_heterochromatic=True),
-                          KmerAndLocation(seq=b'GTAGGGCA', chrom_location=(b'chrom1', 30), is_heterochromatic=True),
-                          KmerAndLocation(seq=b'GTAGGGCA', chrom_location=(b'chrom4', 40), is_heterochromatic=True)],
-            b'TGCCCTAC': [KmerAndLocation(seq=b'TGCCCTAC', chrom_location=(b'chrom2', 40), is_heterochromatic=True)],
-
-            b'GATGGTAG': [KmerAndLocation(seq=b'GATGGTAG', chrom_location=(b'chrom1', 40), is_heterochromatic=True)],
-            b'CTACCATC': [KmerAndLocation(seq=b'CTACCATC', chrom_location=(b'chrom4', 60), is_heterochromatic=True),
-                          KmerAndLocation(seq=b'CTACCATC', chrom_location=(b'chrom4', 70), is_heterochromatic=True)],
-        }
-        primer_combination = [b'GGTAGGAT', b'GTAGGGCA', b'GATGGTAG']
-        pcr_products = get_pcr_products(kmer_locations, primer_combination)
-
-        assert pcr_products[(b'GGTAGGAT', b'GTAGGGCA')] == [(PrimerAndLocation(strand=0, seq=b'GTAGGGCA', chrom_location=(b'chrom1', 30), is_heterochromatic=True),
-                                                             PrimerAndLocation(strand=1, seq=b'ATCCTACC', chrom_location=(b'chrom1', 60), is_heterochromatic=True))]
-
-        print(pcr_products[(b'GGTAGGAT', b'GATGGTAG')])
-        assert pcr_products[(b'GGTAGGAT', b'GATGGTAG')] == [(PrimerAndLocation(strand=0, seq=b'GATGGTAG', chrom_location=(b'chrom1', 40), is_heterochromatic=True),
-                                                             PrimerAndLocation(strand=1, seq=b'ATCCTACC', chrom_location=(b'chrom1', 60), is_heterochromatic=True))]
-
-        assert pcr_products[(b'GTAGGGCA', b'GATGGTAG')] == [(PrimerAndLocation(strand=0, seq=b'GTAGGGCA', chrom_location=(b'chrom4', 40), is_heterochromatic=True),
-                                                             PrimerAndLocation(strand=1, seq=b'CTACCATC', chrom_location=(b'chrom4', 60), is_heterochromatic=True))]
-
-    def test_get_pcr_products2(self):
+    def test_pcr_prducts(self):
         kmer_locations = {
             b'ATT': [KmerAndLocation(seq=b'ATT', chrom_location=(b'1', 0), is_heterochromatic=True),
                      KmerAndLocation(seq=b'ATT', chrom_location=(b'1', 5), is_heterochromatic=True),
