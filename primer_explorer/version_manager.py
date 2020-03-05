5619e8c20c921d2f675f17795b2d0b48c49e7e63
peio <pziarsolo@upv.es>
2020-03-05 12:29:21 +0100

    bugfix: version manager was not executable

--- primer_explorer/version_manager.py
+++ primer_explorer/version_manager.py
@@ -34,7 +34,7 @@ class VersionManager:
     @version.setter
     def version(self, new_version):
         with open(self.version_fpath, 'w') as fhand:
-            fhand.write(f'version = "{new_version}"\n')
+            fhand.write('version = "{new_version}"\n'.format(new_version))
             fhand.flush()
 
     def update_version(self, pre_commit=True):
